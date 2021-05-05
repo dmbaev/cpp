1f4dd272858531d5e1d8123649216693cc37c3c5
Dmitrii Baev <hello@dmbaev.com>
2021-05-05 16:54:12 +0300

    Create main.cpp

---
+++ DecToBin/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string _ourBin;
+string _Dec;
+int main() {
+	
+	cout << "Input dec: ";
+	cin >> _Dec;
+	cout << endl;
+
+	for (auto _l : _Dec)
+	{
+		if (!isdigit(_l))
+		{
+			cout << "Wrong datas";
+			return 0;
+		}
+	}
+	int _Dec_int;
+	_Dec_int = stoi(_Dec);
+
+	while (_Dec_int > 1)
+	{
+		int _i = _Dec_int / 2;
+		_ourBin += to_string(_Dec_int - (_i * 2));
+		_Dec_int = _i;
+	}
+	_ourBin += to_string(_Dec_int);
+	reverse(_ourBin.begin(), _ourBin.end());
+
+	cout << "Dec to bin: " << _ourBin;
+	return stoi(_ourBin);
+}
